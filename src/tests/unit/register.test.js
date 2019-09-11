565866c82c508eae542efe47e79bce82a1b74773
varsnotwars <varsnotwars@protonmail.com>
2019-09-11 16:42:16 +1000

    Clean up tests, using better checks

--- src/tests/unit/register.test.js
+++ src/tests/unit/register.test.js
@@ -3,23 +3,32 @@ import { createServer, createOrmConnection, startServer } from '../../server';
 // TODO: added ignore path to package.json to ignore /dist folder test
 // need to see whether we should be creating test in dist or not/and testing or not
 
-test('creates GraphQLServer', () => {
-  expect(createServer).toBeDefined();
+test('creates and starts GraphQLServer', async () => {
+  const server = createServer();
+  const httpServer = await server.start();
+
+  expect(httpServer.listening).toBe(true);
+
+  httpServer.close();
 });
 
 test('creates typeorm connection', async () => {
   const conn = await createOrmConnection();
 
-  expect(conn).toBeDefined();
+  expect(conn.isConnected).toBe(true);
 
   await conn.close();
 });
 
+// TODO: move this to integration tests
 test('starts server', async () => {
-  const server = await startServer();
+  const { httpServer, typeORMConnection } = await startServer();
 
-  expect(server).toBeDefined();
+  const httpIsListening = httpServer.listening;
+  const ormIsConnected = typeORMConnection.isConnected;
+  
+  expect(httpIsListening && ormIsConnected).toBe(true);
 
-  server.httpServer.close();
-  await server.typeORMConnection.close();
+  httpServer.close();
+  await typeORMConnection.close();
 });
